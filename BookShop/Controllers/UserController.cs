5781a03085b77c73390c28cf72cc06f31fe36fc1
Dhruvil Dobariya <190540107051@darshan.ac.in>
2022-06-28 12:17:09 +0530

    Create Get By Id, Add, Update, and Delete method in User Controller

--- BookShop/Controllers/UserController.cs
+++ BookShop/Controllers/UserController.cs
@@ -17,9 +17,59 @@ namespace BookShop.Controllers
 
         [HttpGet]
         [Route("list")]
-        public async Task<ActionResult<Responce<User>>> GetUsers(int page)
+        public async Task<ActionResult<User>> GetUsers(int page)
         {
-            return (await _crudRepository.GetEntitiesAsync(page));
+            return Ok(await _crudRepository.GetEntitiesAsync(page));
+        }
+
+        [HttpGet]
+        [Route("list/{id}")]
+        public async Task<ActionResult<Responce<User>>> GetUser(int id)
+        {
+            return Ok(await _crudRepository.GetEntityByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> AddUser(User user)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(Json(ModelState));
+            }
+            if (await _crudRepository.AddEntityAsync(user))
+            {
+                return Ok(user);
+            }
+            return BadRequest(_crudRepository.Message);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<User>> UpdateUser(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(Json(ModelState));
+            }
+            if (await _crudRepository.UpdateEntityAsync(user))
+            {
+                return Ok(user);
+            }
+            return BadRequest(_crudRepository.Message);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<ActionResult> DeleteUser(int id)
+        {
+            if(id <= 0)
+            {
+                return BadRequest(Json("Invalid Id"));
+            }
+            if(await _crudRepository.DeleteEntityAsync(id))
+            {
+                return Ok(Json("User deleted successfully"));
+            }
+            return BadRequest(_crudRepository.Message);
         }
     }
 }
